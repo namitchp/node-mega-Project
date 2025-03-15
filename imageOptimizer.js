9875e034bb6d37fd681278564a6c96fe94c509f1
namitchp <namitchp@gmail.com>
2025-03-16 00:00:34 +0530

    image

--- imageOptimizer.js
+++ imageOptimizer.js
@@ -1,30 +1,71 @@
 import multer from 'multer';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 // import fs from 'fs';
 import sharp from 'sharp';
 import * as fs from 'fs';
 import { join } from 'path';
 import path from 'path';
 import { fileURLToPath } from 'url';
+// import { constants } from 'buffer';
 class imageOptimizer {
   uploadImageServer(file) {
     return file;
   }
+  imageUpload() {
+    // const filePath='./uploads/image/original';
+    const fileSize = 20;
+    return multer({
+      storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+          // const {folderName}=req.query;
+          // console.log(req)
+          // Uploads is the Upload_folder_name path
+          const filePath = `./uploads/image/original`;
+          //  if (!fs.existsSync(filePath)) {
+          //       fs.mkdirSync(filePath, { recursive: true });
+          //     }
+          cb(null, filePath);
+        },
+        // dest: filePath,
+        // destination:'./uploads'
+        filename: function (req, file, cb) {
+          const { folderName } = req.query;
+          var ext = path.extname(file.originalname);
+          var filename = path.basename(file.originalname, ext);
+          cb(null, folderName + '-' + filename + '-' + Date.now() + ext);
+        },
+      }),
+      // file size
+      limits: { fileSize: 1024 * 1024 * fileSize },
+      fileFilter: function (req, file, cb) {
+        console.log(file.originalname);
+        console.log(file.mimetype);
+        // Set the filetypes, it is optional
+        var filetypes = /jpeg|jpg|png|pdf/;
+        var mimetype = filetypes.test(file.mimetype);
+        var extname = filetypes.test(
+          path.extname(file.originalname).toLowerCase()
+        );
+        if (mimetype && extname) {
+          return cb(null, true);
+        }
+        cb(
+          'Error: File upload only supports the ' +
+            'following filetypes - ' +
+            filetypes
+        );
+      },
+    });
+  }
   imageGet(query, res) {
-    const imagePath = this.fileAccess(query, 'transformed');
+    const imagePath = this.fileAccess(query, res);
     if (imagePath.valid) {
       return this.transformedImageFun(imagePath.message, res);
     } else {
-      const imagePath = this.fileAccess(query, 'original');
-      if (imagePath.valid) {
-        return this.originalImage(query, imagePath,res);
-      } else {
-        res.status(404).send({ message: 'Image not found' });
-      }
+      return this.originalImage(query, imagePath, res);
     }
   }
-
-  async originalImage(query, imagePath,res) {
+  async originalImage(query, imagePath, res) {
     let contentType;
     let isLossy = false;
     switch (query.format) {
@@ -94,16 +135,8 @@ class imageOptimizer {
       }
     );
   }
-
-
-
-
-
-
-
-
   transformedImageFun(imagePath, res) {
-    console.log(imagePath)
+    console.log(imagePath);
     return res.status(200).sendFile(imagePath, (err) => {
       if (err) {
         res.status(404).send('Image not found');
@@ -111,35 +144,41 @@ class imageOptimizer {
     });
   }
   fetchDirectory(type) {
-    console.log(type);
     const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-// console.log(__dirname)
-    const imagePath = join(__dirname,`./uploads/image/${type}`);
+    const __dirname = path.dirname(__filename); // get the name of the directory
+    // console.log(__dirname)
+    const imagePath = join(__dirname, `./uploads/image/${type}`);
     return imagePath;
   }
-  fileAccess(reqObj, type) {
-    const fileName = `name=${reqObj.name}&width=${reqObj.width}&height=${reqObj.height}&quality=${reqObj.quality}.${reqObj.format}`;
-    const accessFolder = `${this.fetchDirectory(type)}/${
-      type === 'original' ? reqObj.name : fileName
-    }`;
+  fileAccess(reqObj, res) {
+    const { name, width, height, quality, format } = reqObj;
+    const fileName = `name=${name}&width=${width}&height=${height}&quality=${quality}.${format}`;
+    let accessFolder = `${this.fetchDirectory('transformed')}/${fileName}`;
     if (fs.existsSync(accessFolder)) {
       return { message: accessFolder, valid: true, fileName };
-    } else {
+    }
+    accessFolder = `${this.fetchDirectory('original')}/${name}`;
+    if (fs.existsSync(accessFolder)) {
       return { message: accessFolder, valid: false, fileName };
     }
+    res.status(404).send({ message: 'Image not found' });
   }
 }
-
 export const imageOptimizationFun = (app) => {
   const classOptimization = new imageOptimizer();
-  app.get('/upload', (req, res) => {
-    
-    console.log('hello');
-    classOptimization.imageGet(req.query, res);
-  });
+  app.post(
+    '/upload',
+    classOptimization.imageUpload().single('file'),
+    (req, res) => {
+      res.status(200).json({
+        message: 'Success',
+        data: req.file,
+      });
+      // console.log('hello');
+      // classOptimization.imageGet(req.query, res);
+    }
+  );
   app.get('/image', (req, res) => {
-    console.log('hello');
     classOptimization.imageGet(req.query, res);
   });
 };
